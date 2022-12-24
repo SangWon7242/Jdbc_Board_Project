5436f0f84fd65615fcafa13fd0e181f72222db3d
SangWon7242 <gotkdqja@gmail.com>
2022-12-24 10:36:38 +0900

    게시물 작성시 DB에 저장

--- src/com/sbs/exam/textboard/Main.java
+++ src/com/sbs/exam/textboard/Main.java
@@ -1,3 +1,5 @@
+package com.sbs.exam.textboard;
+
 public class Main {
   public static void main(String[] args) {
     new App().run();
--- src/com/sbs/exam/textboard/test/JDBCInsertTest.java
+++ src/com/sbs/exam/textboard/test/JDBCInsertTest.java
@@ -1,3 +1,5 @@
+package com.sbs.exam.textboard.test;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
