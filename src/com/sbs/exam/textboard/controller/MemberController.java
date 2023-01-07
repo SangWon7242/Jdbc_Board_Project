e1a94201e4382bbb092385f2c72fb34e4f1b429b
SangWon7242 <gotkdqja@gmail.com>
2023-01-07 10:58:34 +0900

    서비스와 DAO 구조 도입

--- src/com/sbs/exam/textboard/controller/MemberController.java
+++ src/com/sbs/exam/textboard/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.sbs.exam.textboard.controller;
 
+import com.sbs.exam.textboard.service.MemberService;
 import com.sbs.exam.textboard.util.DBUtil;
 import com.sbs.exam.textboard.util.SecSql;
 
@@ -7,6 +8,12 @@ import java.sql.Connection;
 import java.util.Scanner;
 
 public class MemberController extends Controller {
+  private MemberService memberService;
+
+  public MemberController(Connection conn, Scanner sc) {
+    super(sc);
+    memberService = new MemberService(conn);
+  }
 
   public void join(String cmd) {
     String loginId;
@@ -26,15 +33,9 @@ public class MemberController extends Controller {
         continue;
       }
 
-      SecSql sql = new SecSql();
+      boolean isLoginedDup = memberService.isLoginedDup(loginId);
 
-      sql.append("SELECT COUNT(*) > 0");
-      sql.append("FROM `member`");
-      sql.append("WHERE loginId = ?", loginId);
-
-      boolean loginedDup = DBUtil.selectRowBooleanValue(conn, sql);
-
-      if ( loginedDup ) {
+      if ( isLoginedDup ) {
         System.out.printf("%s(은)는 이미 사용중인 로그인 아이디입니다.\n", loginId);
         continue;
       }
@@ -89,15 +90,7 @@ public class MemberController extends Controller {
       break;
     }
 
-    SecSql sql = new SecSql();
-    sql.append("INSERT INTO `member`");
-    sql.append("SET regDate = NOW()");
-    sql.append(", updateDate = NOW()");
-    sql.append(", loginId = ?", loginId);
-    sql.append(", loginPw = ?", loginPw);
-    sql.append(", name = ?", name);
-
-    int id = DBUtil.insert(conn, sql);
+    int id = memberService.join(loginId, loginPw, name);
     System.out.printf("%d번 회원이 생성 되었습니다.\n", id);
   }
 }
